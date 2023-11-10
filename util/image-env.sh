ea5265b75b339dc1d04622a92c8e8499513b0757
cvicens <carlos.vicens.alonso@gmail.com>
2023-11-10 12:22:03 +0100

    find added

--- util/image-env.sh
+++ util/image-env.sh
@@ -3,5 +3,5 @@
 export REGISTRY=quay.io
 export REGISTRY_USER_ID=atarazana
 export IMAGE_NAME=argocd-tools
-export IMAGE_VERSION=0.0.1
+export IMAGE_VERSION=0.0.2
 export FROM_IMAGE=registry.redhat.io/ubi8/ubi-minimal:8.8-1072.1697626218
